6d96214761547ba5f623410216fccda109a00baa
Havy <hnakalyowa29@gmail.com>
2025-05-06 07:11:31 -0700

    Refactor variable declarations and enhance object property access in index.js

--- CSS/JS _BASICS/index.js
+++ CSS/JS _BASICS/index.js
@@ -1,9 +1,25 @@
 console.log("Hello World");
-let name ="Havy";//string literal
+let name = "Havy";//string literal
 console.log(name);
-let firstName ="Havy";
+let firstName = "Havy";
 let lastName = "Nakalyowa";
 let age = 23;//number literal
 let isStudent = true; //boolean literal
-let firstName1 =undefined;
-let lastName1 =null;
+let firstName1 = undefined;
+let lastName1 = null;
+// reference types
+//objects,arrays,functions
+//objects
+let person = {
+    firstName: "Havy",
+    lastName: "Nakalyowa",
+    age: 23
+};
+console.log(person);
+//accessing object properties
+//dot notation
+person.firstName = "Havyd";
+console.log(person.firstName);
+//bracket notation
+person["lastName"] = "Naka";
+console.log(person["lastName"]);
