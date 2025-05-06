427c53db2a4472387514bf2427fd643002f27c8a
Havy <hnakalyowa29@gmail.com>
2025-05-06 06:38:44 -0700

    Add age and isStudent variables to index.js

--- CSS/JS _BASICS/index.js
+++ CSS/JS _BASICS/index.js
@@ -3,3 +3,7 @@ let name ="Havy";//string literal
 console.log(name);
 let firstName ="Havy";
 let lastName = "Nakalyowa";
+let age = 23;//number literal
+let isStudent = true; //boolean literal
+let firstName1 =undefined;
+let lastName1 =null;
