53e24a4c6d71d042fd1fa963dfd14832080d58bb
Havy <hnakalyowa29@gmail.com>
2025-05-06 07:33:53 -0700

    Add array initialization and element access in index.js

--- CSS/JS _BASICS/index.js
+++ CSS/JS _BASICS/index.js
@@ -23,3 +23,14 @@ console.log(person.firstName);
 //bracket notation
 person["lastName"] = "Naka";
 console.log(person["lastName"]);
+
+//arrays
+let numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
+//accessing array elements
+console.log(numbers[0]);
+
+//fumctions
+
+
+
