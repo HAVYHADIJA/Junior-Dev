8d5ee4f3d9f09a8769560dfc8cddb7eb7ddc826d
Havy <hnakalyowa29@gmail.com>
2025-07-16 01:52:16 -0700

    implement __str__ method for Book class to enhance string representation

--- book.py
+++ book.py
@@ -42,4 +42,22 @@ odyssey = Book("The Odyssey", "Homer")
 
 print(repr(odyssey))
 print(str(odyssey))
+# book.py
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def __repr__(self):
+        class_name = type(self).__name__
+        return f"{class_name}(title={self.title!r}, author={self.author!r})"
+
+    def __str__(self):
+        return f'"{self.title}" by {self.author}'
+
+odyssey = Book("The Odyssey", "Homer")
+
+print(repr(odyssey))
+print(str(odyssey))
 
